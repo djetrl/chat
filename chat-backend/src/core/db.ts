efebfe06bee3bb1c2a11fb7d231fc1cdd9d37683
djetrl <leshadjet@Mail.ru>
2024-05-19 09:02:44 +0700

    firs commit

--- chat-backend/src/core/db.ts
+++ chat-backend/src/core/db.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/chat', {
+mongoose.connect('mongodb://chat-db:27017/chat', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
