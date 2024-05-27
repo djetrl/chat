ea2721df88ec330105815a16a28d3e4a276365ba
djetrl <leshadjet@Mail.ru>
2024-05-28 03:54:06 +0700

    fix dockerfile chat-frontend

--- chat-fontend/src/core/axios.js
+++ chat-fontend/src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import getCookie from '../utils/helpers/getCookie' ;
-axios.defaults.baseURL = 'http://172.17.2.98:3003';
+axios.defaults.baseURL = 'http://localhost:3003';
 axios.defaults.headers.common['token'] = getCookie('acsTKn');
 axios.defaults.headers.common['Accept'] = "*"
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = "*"
